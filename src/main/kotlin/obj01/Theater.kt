4dee94b791abd6f2b9f6b252bc3a7967faf2ace0
sjjeong <sjjeong1225@gmail.com>
2021-06-01 16:06:55 +0900

    TicketSeller로 로직 이동

--- src/main/kotlin/obj01/Theater.kt
+++ src/main/kotlin/obj01/Theater.kt
@@ -3,14 +3,6 @@ package obj01
 class Theater(private val ticketSeller: TicketSeller) {
 
     fun enter(audience: Audience) {
-        val ticket = ticketSeller.ticketOffice.ticket ?: return
-        if (audience.bag.hasInvitation) {
-            audience.bag.ticket = ticket
-        } else {
-            val fee = ticket.fee
-            audience.bag.minusAmount(fee)
-            audience.bag.ticket = ticket
-            ticketSeller.ticketOffice.plusAmount(fee)
-        }
+        ticketSeller.sellTo(audience)
     }
 }
--- src/main/kotlin/obj01/TicketSeller.kt
+++ src/main/kotlin/obj01/TicketSeller.kt
@@ -1,3 +1,15 @@
 package obj01
 
-class TicketSeller(val ticketOffice: TicketOffice)
+class TicketSeller(private val ticketOffice: TicketOffice) {
+    fun sellTo(audience: Audience) {
+        val ticket = ticketOffice.ticket ?: return
+        if (audience.bag.hasInvitation) {
+            audience.bag.ticket = ticket
+        } else {
+            val fee = ticket.fee
+            audience.bag.minusAmount(fee)
+            audience.bag.ticket = ticket
+            ticketOffice.plusAmount(fee)
+        }
+    }
+}
